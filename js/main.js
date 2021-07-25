f8f73e9da30d1191acf94f536611346f2b5a95a4
Abhishek Mankuskar <mankuskarabhi2@gmail.com>
2021-07-25 08:43:45 +0530

    updte

---
+++ js/main.js
@@ -0,0 +1,264 @@
+(function($) {
+
+	"use strict";
+
+   $(window).load(function() {
+
+      // will first fade out the loading animation
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      });
+
+  	})
+
+
+  	/*---------------------------------------------------- */
+  	/* FitText Settings
+  	------------------------------------------------------ */
+  	setTimeout(function() {
+
+   	$('#intro h1').fitText(1, { minFontSize: '42px', maxFontSize: '84px' });
+
+  	}, 100);
+
+
+	/*---------------------------------------------------- */
+	/* FitVids
+	------------------------------------------------------ */
+  	$(".fluid-video-wrapper").fitVids();
+
+
+	/*----------------------------------------------------- */
+	/* Alert Boxes
+  	------------------------------------------------------- */
+	$('.alert-box').on('click', '.close', function() {
+	  $(this).parent().fadeOut(500);
+	});
+
+
+	/*----------------------------------------------------- */
+	/* Stat Counter
+  	------------------------------------------------------- */
+   var statSection = $("#stats"),
+       stats = $(".stat-count");
+
+   statSection.waypoint({
+
+   	handler: function(direction) {
+
+      	if (direction === "down") {
+
+			   stats.each(function () {
+				   var $this = $(this);
+
+				   $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+				   	duration: 4000,
+				   	easing: 'swing',
+				   	step: function (curValue) {
+				      	$this.text(Math.ceil(curValue));
+				    	}
+				  	});
+				});
+
+       	}
+
+       	// trigger once only
+       	this.destroy();
+
+		},
+
+		offset: "90%"
+
+	});
+
+
+	/*---------------------------------------------------- */
+	/*	Masonry
+	------------------------------------------------------ */
+	var containerProjects = $('#folio-wrapper');
+
+	containerProjects.imagesLoaded( function() {
+
+		containerProjects.masonry( {
+		  	itemSelector: '.folio-item',
+		  	resize: true
+		});
+
+	});
+
+	/*-----------------------------------------------------*/
+  	/* Navigation Menu
+   ------------------------------------------------------ */
+//    var toggleButton = $('.menu-toggle'),
+//        nav = $('.main-navigation');
+
+//    // toggle button
+//    toggleButton.on('click', function(e) {
+
+// 		e.preventDefault();
+// 		toggleButton.toggleClass('is-clicked');
+// 		nav.slideToggle();
+
+// 	});
+
+//    // nav items
+//   	nav.find('li a').on("click", function() {
+
+//    	// update the toggle button
+//    	toggleButton.toggleClass('is-clicked');
+//    	// fadeout the navigation panel
+//    	nav.fadeOut();
+
+//   	});
+
+const navSlide = ()=>{
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('nav ul')
+    const navLink = document.querySelectorAll('nav ul li')
+    burger.onclick = ()=>{
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('active');
+        navLink.forEach((link,index)=>{
+            if(link.style.animation){
+                link.style.animation = ''
+            }
+            else{
+                link.style.animation = `navFade 0.5s ease forwards ${index/7 + 0.5}s`
+            }
+        })
+    }
+}
+navSlide();
+
+   /*---------------------------------------------------- */
+  	/* Highlight the current section in the navigation bar
+  	------------------------------------------------------ */
+	var sections = $("section"),
+	navigation_links = $("#main-nav-wrap li a");
+
+	sections.waypoint( {
+
+       handler: function(direction) {
+
+		   var active_section;
+
+			active_section = $('section#' + this.element.id);
+
+			if (direction === "up") active_section = active_section.prev();
+
+			var active_link = $('#main-nav-wrap a[href="#' + active_section.attr("id") + '"]');
+
+         navigation_links.parent().removeClass("current");
+			active_link.parent().addClass("current");
+
+		},
+
+		offset: '25%'
+	});
+
+
+	/*---------------------------------------------------- */
+  	/* Smooth Scrolling
+  	------------------------------------------------------ */
+  	$('.smoothscroll').on('click', function (e) {
+
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+
+
+ 	/*----------------------------------------------------- */
+  	/* Back to top
+   ------------------------------------------------------- */
+	var pxShow = 300; // height on which the button will show
+	var fadeInTime = 400; // how slow/fast you want the button to show
+	var fadeOutTime = 400; // how slow/fast you want the button to hide
+	var scrollSpeed = 300; // how slow/fast you want the button to scroll to top. can be a value, 'slow', 'normal' or 'fast'
+
+   // Show or hide the sticky footer button
+	jQuery(window).scroll(function() {
+
+		if (!( $("#header-search").hasClass('is-visible'))) {
+
+			if (jQuery(window).scrollTop() >= pxShow) {
+				jQuery("#go-top").fadeIn(fadeInTime);
+			} else {
+				jQuery("#go-top").fadeOut(fadeOutTime);
+			}
+
+		}
+
+	});
+
+})(jQuery);
+
+
+
+
+
+
+
+
+//   all ------------------
+function initParadoxWay() {
+    "use strict";
+   
+    if ($(".testimonials-carousel").length > 0) {
+        var j2 = new Swiper(".testimonials-carousel .swiper-container", {
+            preloadImages: false,
+            slidesPerView: 1,
+            spaceBetween: 20,
+            loop: true,
+            grabCursor: true,
+            mousewheel: false,
+            centeredSlides: true,
+            pagination: {
+                el: '.tc-pagination',
+                clickable: true,
+                dynamicBullets: true,
+            },
+            navigation: {
+                nextEl: '.listing-carousel-button-next',
+                prevEl: '.listing-carousel-button-prev',
+            },
+            breakpoints: {
+                1024: {
+                    slidesPerView: 3,
+                },
+                
+            }
+        });
+    }
+    
+// bubbles -----------------
+    
+    
+    setInterval(function () {
+        var size = randomValue(sArray);
+        $('.bubbles').append('<div class="individual-bubble" style="left: ' + randomValue(bArray) + 'px; width: ' + size + 'px; height:' + size + 'px;"></div>');
+        $('.individual-bubble').animate({
+            'bottom': '100%',
+            'opacity': '-=0.7'
+        }, 4000, function () {
+            $(this).remove()
+        });
+    }, 350);
+    
+}
+
+//   Init All ------------------
+$(document).ready(function () {
+    initParadoxWay();
+});
